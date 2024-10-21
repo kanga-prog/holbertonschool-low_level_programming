c4ba83629838908809663afd5c335fc57f296f63
kanga-prog <9952@holbertonstudents.com>
2024-10-21 20:36:58 +0200

    EXO6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,8 +4,12 @@
 * main - entry point
 * Return: return 0 if success
 */
-int main(void) 
+int main(void)
 {
-printf("char: %zu, int: %zu, float: %zu, double: %zu\n", sizeof(char), sizeof(int), sizeof(float), sizeof(double));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return 0;
 }
