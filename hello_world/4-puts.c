4e3ab7578cd011f7ffa1335d1107e9af1b707ebb
kanga-prog <9952@holbertonstudents.com>
2024-10-21 15:34:05 +0200

    EXO4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ *function_nain-La fonction main est le point d'entrée d'un programme en C .
+ *function_return 0-La ligne return 0; dans une fonction main en C sert à indiquer que le programme s'est terminé avec succès.
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
