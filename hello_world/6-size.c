fe9f4391b2bca640ac444355b909c8af3dfaa309
kanga-prog <9952@holbertonstudents.com>
2024-10-21 20:26:29 +0200

    EXO 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: return 0 if success
+*/
+int main(void) 
+{
+printf("char: %zu, int: %zu, float: %zu, double: %zu\n", sizeof(char), sizeof(int), sizeof(float), sizeof(double));
+return 0;
+}
