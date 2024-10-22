7e6bfbc05ab908c3e5c461ab59f12f7080992689
kanga-prog <9952@holbertonstudents.com>
2024-10-22 13:05:20 +0200

    EXO 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ *
+ * more headers goes there
+ *
+ * betty style doc for function main goes there
  */
+
 int main(void)
 {
-	int n = 0;
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
 
 	if (n > 0)
 	{
