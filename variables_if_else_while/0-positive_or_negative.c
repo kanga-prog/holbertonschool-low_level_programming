0492fa2fb7c376dcc1286f102c991dab3a2cbe21
kanga-prog <9952@holbertonstudents.com>
2024-10-22 12:11:05 +0200

    EXO 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	if (n < 0)
+	{
+		printf("is negative\n");
+	}
+
+	return (0);
+}
