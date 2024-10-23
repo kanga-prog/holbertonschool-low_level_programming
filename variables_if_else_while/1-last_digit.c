822f57dd4ba18b612e358c88a17d0d0e8e460b88
kanga-prog <9952@holbertonstudents.com>
2024-10-23 12:58:26 +0200

    EXO 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- *
+ * main - Entry point
+ * Description: Generates random number and prints the last digit
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int Last_digit;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	Last_digit = abs(n % 10);
-
-	printf("Last digit of %d is", n);
-
-	if (Last_digit > 5)
-		printf(" %d and is greater than 5\n", Last_digit);
-	else if (Last_digit == 0)
-		printf(" %d and is 0\n", Last_digit);
-	else
-		printf(" %d and is less than 6 and not 0\n", Last_digit);
-
-	return (0);
-}
-
+        int n;
+        int last_digit;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        last_digit = n % 10;
+        /* Print the last digit and the corresponding message */
+        printf("Last digit of %d is %d and is ", n, last_digit);
+        if (last_digit > 5)
+        {
+                printf("greater than 5\n");
+        }
+        else if (last_digit == 0)
+        {
+                printf("0\n");
+        }
+        else
+        {
+                printf("less than 6 and not 0\n");
+        }
+        return (0);
