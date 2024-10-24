e248c646410e20033ae6f8a5f3e18409e81949f6
kanga-prog <9952@holbertonstudents.com>
2024-10-24 16:50:38 +0200

    EXO 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,8 @@
-#include "main.h"
+#include <unistd.h> 
 
-/**
- * _putchar - writes a character to stdout
- * @c: The character to print
- * Description: _putchar is generally used to write a character.
- * Prototype: This function indicates its name, return type, and parameters.
- * Return: Always 0 (Success)
- */
+
 int _putchar(char c)
 {
-	write(1, &c, 1);
-	return (0);
+	return write(1, &c, 1);	
+	
 }
