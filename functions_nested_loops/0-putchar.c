8e20511de7a6f862c2246dd0f3221ab0ff85157f
kanga-prog <9952@holbertonstudents.com>
2024-10-24 12:54:37 +0200

    EXO 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates random number and prints the last digit
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("_putchar\n");
+    return 0;
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Ce README traite des quetions sur la fonction et les argument de fonction
