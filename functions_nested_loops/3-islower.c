aeb85285c366233b5a76d11a64be18c6be749222
kanga-prog <9952@holbertonstudents.com>
2024-10-25 15:32:14 +0200

    EXO 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _islower - Checks if the character is lowercase.
+ * @c: The character to be checked.
  *
- * Return: Always 0.
+ * Return: 1 if c is lowercase, 0 otherwise.
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-
-	return (0);
+	if (c >= 'a' && c <= 'z')  /* Check if c is between 'a' and 'z' */
+	{
+		return (1);  /* c is lowercase */
+	}
+	return (0);  /* c is not lowercase */
 }
