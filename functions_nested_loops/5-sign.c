0aed897031858d77f5876988ba42c3b094968a9b
kanga-prog <9952@holbertonstudents.com>
2024-10-26 12:41:22 +0200

    EXO5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - Prints the sign of a number.
@@ -12,17 +13,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+"); /* Print '+' for positive numbers */
-		return (1);  /* Return 1 */
+		putchar(+);
+		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0"); /* Print '0' for zero */
-		return (0);  /* Return 0 */
+		putchar(0);
+		return (0);
 	}
 	else
 	{
-		printf("-"); /* Print '-' for negative numbers */
-		return (-1); /* Return -1 */
+		putchar(-);
+		return (-1);
 	}
 }
