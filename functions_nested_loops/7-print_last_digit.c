78dbf3ce58d326c572f16ecc5c1dbfa09db20a68
kanga-prog <9952@holbertonstudents.com>
2024-10-26 16:50:58 +0200

    EXO7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -10,6 +10,7 @@
 int print_last_digit(int n)
 {
 	int last_digit;
+	char output;
 
 	last_digit = n % 10; /* Get the last digit */
 	if (last_digit < 0)
@@ -17,6 +18,7 @@ int print_last_digit(int n)
 		last_digit = -last_digit; /* Make it positive if it's negative */
 	}
 
-	putchar(last_digit + '0'); /* Print the last digit as a character */
+	output = last_digit + '0'; /* Convert last digit to character */
+	write(1, &output, 1);       /* Print the last digit as a character */
 	return (last_digit);         /* Return the last digit */
 }
