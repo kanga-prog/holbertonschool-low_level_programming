1773bdb8d1ff47df806a2bb4471f402fdb85be87
kanga-prog <9952@holbertonstudents.com>
2024-10-26 13:59:35 +0200

    EXO5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -16,14 +16,16 @@ int print_sign(int n)
 		putchar(+);
 		return (1);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		putchar(0);
 		return (0);
 	}
-	else
+	if (n > 0)
 	{
 		putchar(-);
 		return (-1);
 	}
+
+
 }
