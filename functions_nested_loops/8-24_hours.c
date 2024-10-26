0251d1014fe5488080eb5dba3c8de8eb316d4119
kanga-prog <9952@holbertonstudents.com>
2024-10-26 16:59:37 +0200

    EXO9

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * jack_bauer - Prints every minute of Jack Bauer's day.
@@ -7,12 +7,21 @@ void jack_bauer(void)
 {
 	int hour;   /* Variable for hours */
 	int minute; /* Variable for minutes */
+	char buffer[6]; /* Buffer to hold the time in HH:MM format */
 
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			printf("%02d:%02d\n", hour, minute); /* Print time in HH:MM format */
+			/* Format the time into the buffer */
+			buffer[0] = (hour / 10) + '0';  /* Tens of hour */
+			buffer[1] = (hour % 10) + '0';  /* Units of hour */
+			buffer[2] = ':';                 /* Colon separator */
+			buffer[3] = (minute / 10) + '0';/* Tens of minute */
+			buffer[4] = (minute % 10) + '0';/* Units of minute */
+			buffer[5] = '\n';                /* Newline character */
+
+			write(1, buffer, sizeof(buffer)); /* Write the buffer to stdout */
 		}
 	}
 }
