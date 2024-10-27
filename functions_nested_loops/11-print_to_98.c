a35058bcc962e27339d732d3e9fd6bfca087a512
kanga-prog <9952@holbertonstudents.com>
2024-10-27 16:52:54 +0100

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
 
-void _putchar(char c);
-
-/**
- * print_number - Prints an integer
- * @n: The integer to print
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar((n % 10) + '0');
-}
+void print_to_98(int n);
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
@@ -30,11 +12,10 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			print_number(n);
+			printf("%d", n);
 			if (n < 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf(", ");
 			}
 		}
 	}
@@ -42,33 +23,12 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			print_number(n);
+			printf("%d", n);
 			if (n > 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf(", ");
 			}
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
-
-/**
- * _putchar - Outputs a character
- * @c: The character to print
- */
-void _putchar(char c)
-{
-	putchar(c);
-}
-
-/**
- * main - Entry point for the program
- * Return: Always 0
- */
-int main(void)
-{
-	print_to_98(95);  /* Example usage */
-	return (0);
-}
-
