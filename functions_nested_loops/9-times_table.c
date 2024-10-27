f4b712342b432405bf4e5c6abfc0125381d7e7f2
kanga-prog <9952@holbertonstudents.com>
2024-10-27 17:13:05 +0100

    EXO9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,23 +22,20 @@ void times_table(void)
 			{
 				buffer[0] = result + '0'; /* Single digit */
 				len = 1;
-				buffer[1] = ' ';           /* Space after single digit */
-				buffer[2] = '\0';          /* Null terminate */
 			}
 			else
 			{
 				buffer[0] = (result / 10) + '0'; /* Tens place */
 				buffer[1] = (result % 10) + '0'; /* Units place */
 				len = 2;
-				buffer[2] = ' ';               /* Space after two digits */
-				buffer[3] = '\0';              /* Null terminate */
 			}
+			buffer[len] = '\0'; /* Null terminate */
 
-			write(1, buffer, len + 1); /* Write the result to stdout */
+			write(1, buffer, len); /* Write the result to stdout */
 
 			if (j < 9) /* Avoid adding a comma after the last number */
 			{
-				write(1, ",  ", 4); /* Write a comma and two spaces */
+				write(1, ", ", 2); /* Write a comma and space */
 			}
 		}
 		write(1, "\n", 1); /* Newline after each row is printed */
