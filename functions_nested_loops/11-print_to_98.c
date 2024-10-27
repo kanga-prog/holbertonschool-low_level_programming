c699edf71815613d53092c4848a134e189e769db
kanga-prog <9952@holbertonstudents.com>
2024-10-27 16:13:33 +0100

    EXO11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point for the program
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_to_98(95);
+	return (0);
+}
