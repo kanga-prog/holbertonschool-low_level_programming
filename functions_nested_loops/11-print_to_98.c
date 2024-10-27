32c5a50c4c1165e5f285101b7628a41a0b8300de
kanga-prog <9952@holbertonstudents.com>
2024-10-27 16:26:57 +0100

    EXO 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,63 @@
 #include <stdio.h>
 
 /**
+ * _putchar - Outputs a character
+ * @c: The character to print
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
+
+/**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The starting number
  */
 void print_to_98(int n)
 {
-	int i;  /* Declare loop variable outside */
-
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-			printf("%d", i);
-			if (i < 98)
+			print_number(n);
+			if (n < 98)
 			{
-				printf(", ");
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (; n >= 98; n--)
 		{
-			printf("%d", i);
-			if (i > 98)
+			print_number(n);
+			if (n > 98)
 			{
-				printf(", ");
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
-	printf("\n");
+	_putchar('\n');
 }
 
 /**
