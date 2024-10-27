d069118e5bfc3883834f0e86ada8e611dcaebe07
kanga-prog <9952@holbertonstudents.com>
2024-10-27 17:02:32 +0100

    EXO9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,14 +1,15 @@
 #include <unistd.h>
 
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ * print_multiplication_table - Prints the multiplication table from 0 to 9.
+ *
+ * Return: void
  */
-void times_table(void)
+void print_multiplication_table(void)
 {
-	int i;          /* Variable for the row */
-	int j;          /* Variable for the column */
-	char buffer[5]; /* Buffer to hold the output string */
+	char buffer[6]; /* Buffer to hold the output string */
 	int len;        /* Length of the output string */
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -21,19 +22,23 @@ void times_table(void)
 			{
 				buffer[0] = result + '0'; /* Single digit */
 				len = 1;
+				buffer[1] = ' ';           /* Space after single digit */
+				buffer[2] = '\0';          /* Null terminate */
 			}
 			else
 			{
 				buffer[0] = (result / 10) + '0'; /* Tens place */
 				buffer[1] = (result % 10) + '0'; /* Units place */
 				len = 2;
+				buffer[2] = ' ';               /* Space after two digits */
+				buffer[3] = '\0';              /* Null terminate */
 			}
 
-			write(1, buffer, len); /* Write the result to stdout */
+			write(1, buffer, len + 1); /* Write the result to stdout */
 
 			if (j < 9) /* Avoid adding a comma after the last number */
 			{
-				write(1, ", ", 2); /* Write a comma and space */
+				write(1, ",  ", 4); /* Write a comma and two spaces */
 			}
 		}
 		write(1, "\n", 1); /* Newline after each row is printed */
