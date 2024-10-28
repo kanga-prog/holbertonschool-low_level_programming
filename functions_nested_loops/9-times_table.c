f5b7570d96d3f19e6d49862e5d26bdbc05c954ce
kanga-prog <9952@holbertonstudents.com>
2024-10-28 14:13:13 +0100

    EXO 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,46 +1,36 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * times_table - Prints the multiplication table from 0 to 9.
+ * print_9_times_table - Prints the 9 times table.
  *
  * Return: void
  */
-void times_table(void)
+void print_9_times_table(void)
 {
-	char buffer[6]; /* Buffer to hold the output string */
-	int len;        /* Length of the output string */
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			int result = i * j; /* Calculate the result */
-
-			/* Convert the result to a string */
-			if (result < 10)
+			printf("%2d", i * j);
+			if (j < 9)
 			{
-				buffer[0] = result + '0'; /* Single digit */
-				len = 1;
-				buffer[1] = ' ';           /* Space after single digit */
-				buffer[2] = '\0';          /* Null terminate */
-			}
-			else
-			{
-				buffer[0] = (result / 10) + '0'; /* Tens place */
-				buffer[1] = (result % 10) + '0'; /* Units place */
-				len = 2;
-				buffer[2] = ' ';               /* Space after two digits */
-				buffer[3] = '\0';              /* Null terminate */
-			}
-
-			write(1, buffer, len + 1); /* Write the result to stdout */
-
-			if (j < 9) /* Avoid adding a comma after the last number */
-			{
-				write(1, ",  ", 4); /* Write a comma and two spaces */
+				printf(",  ");
 			}
 		}
-		write(1, "\n", 1); /* Newline after each row is printed */
+		printf("$\n");
 	}
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_9_times_table();
+	return (0);
 }
