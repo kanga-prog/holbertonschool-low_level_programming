0a12ac4db1da38bd05c21d70240b9fb70072159e
kanga-prog <9952@holbertonstudents.com>
2024-10-28 16:24:24 +0100

    EXO 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
- * print_9_times_table - Prints the 9 times table.
- *
- * Return: void
- */
-void print_9_times_table(void)
+* times_table - prints the 9 times table
+*
+* Return: 0 if succes
+*/
+void times_table(void)
 {
-	int i;
-	int j;
+	int i, j, k, l, m;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			printf("%2d", i * j);
+			k = i * j;
+			l = k / 10;
+			m = k % 10;
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (k <= 9)
+			{
+				_putchar(' ');
+				_putchar(m + '0');
+			}
+			else
+			{
+				_putchar(l + '0');
+				_putchar(m + '0');
+			}
 			if (j < 9)
 			{
-				printf(",  ");
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
 			}
 		}
-		printf("$\n");
 	}
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0
- */
-int main(void)
-{
-	print_9_times_table();
-	return (0);
 }
