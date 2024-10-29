e3034a99384385324fac71bab53fefd9a1613545
kanga-prog <9952@holbertonstudents.com>
2024-10-29 08:01:55 +0100

    EXO0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-/* Prototype for the _isupper function */
-int _isupper(int c);
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+	{
+	/* Check if the character is between 'A' and 'Z' */
+	if (c >= 'A' && c <= 'Z')
+	{
+	putchar('1'); /* Print '1' if c is uppercase */
+	}
+	else
+	{
+	putchar('0'); /* Print '0' otherwise */
+	}
+
+	return (0); /* Return 0 as a general exit status (not used here) */
+}
 
 /**
  * main - Entry point of the program
@@ -10,16 +28,16 @@ int _isupper(int c);
  */
 int main(void)
 {
+	/* Test the function with different characters */
 	char test_chars[] = {'A', 'a', 'Z', '1', '!', 'M'};
-	char c;
 	int i;
 
 	for (i = 0; i < sizeof(test_chars) / sizeof(test_chars[0]); i++)
 	{
-	c = test_chars[i];
-	printf("%c: ", c);
-	_isupper(c);  /* Call to the _isupper function */
-	putchar('\n'); /* New line for better output readability */
+	putchar(test_chars[i]); /* Print the character being tested */
+	putchar(':');           /* Print a separator */
+	_isupper(test_chars[i]); /* Call the _isupper function */
+	putchar('\n');         /* New line for better output readability */
 	}
 
 	return (0);
