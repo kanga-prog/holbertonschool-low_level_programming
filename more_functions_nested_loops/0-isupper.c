a77ee6248e8265ce2ae6dced4bebef7169472804
kanga-prog <9952@holbertonstudents.com>
2024-10-29 13:13:34 +0100

    EXO 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,38 @@
+#include<stdio.h>
 #include "main.h"
 
 /**
- * _isupper - Checks if the character isuppercase.
+ * _isupper - Checks if the character is uppercase.
  * @c: The character to be checked.
  *
  * Return: 1 if c is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'A') /* Check if c is between 'A' and 'Z' */
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
-		return (0);
+	return (0);
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+	putchar(letter);
+	putchar(':');
+	putchar(' ');
+	putchar(_isupper(letter) + '0');
+	putchar('\n');
+	}
+
+	return (0);
 }
