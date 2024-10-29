01ce5ff32181a9b9698cb9b77196103e6600135e
kanga-prog <9952@holbertonstudents.com>
2024-10-29 13:01:45 +0100

    EXO 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _islower - Checks if the character is lowercase.
+ * _isupper - Checks if the character isuppercase.
  * @c: The character to be checked.
  *
- * Return: 1 if c is lowercase, 0 otherwise.
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
-int _islower(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'A') /* Check if c is between 'A' and 'Z' */
 	{
-		return (1); /* c is lowercase */
+		return (1);
 	}
-		return (0); /* c is not lowercase */
+		return (0);
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,10 @@
 #ifndef main_h
 #define main_h
 
-
+/*
+ * File: main.h
+ * Auth: KANGA KOUAKOU BRICE
+ * Desc: Header file containing declarations for all functions
+ */
 int _isupper(int c);
-
-
 #endif
