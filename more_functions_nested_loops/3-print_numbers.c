194646dd89d082c3eefd47da209b69ea3a1737cd
kanga-prog <9952@holbertonstudents.com>
2024-10-29 14:23:39 +0100

    EXO3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Prints numbers from 0 to 9 on the same line.
+ *
+ * Return: Nothing.
+ */
+void print_number(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+}
