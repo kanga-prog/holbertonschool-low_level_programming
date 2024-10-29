dd6d0ba2927c444714913c3a291a8afbdf6ff317
kanga-prog <9952@holbertonstudents.com>
2024-10-29 08:39:22 +0100

    EXO0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,44 +1,16 @@
-#include <stdio.ih>
-#include <stddef.h>
+#include "main.h"
 
 /**
- * _isupper - checks if a character is uppercase
- * @c: character to check
+ * _islower - Checks if the character is lowercase.
+ * @c: The character to be checked.
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is lowercase, 0 otherwise.
  */
-int _isupper(int c)
+int _islower(int c)
 {
-	/* Check if the character is between 'A' and 'Z' */
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'A') /* Check if c is between 'A' and 'Z' */
 	{
-	return (1);
+		return (1); /* c is lowercase */
 	}
-	else
-	{
-	return ('0');
-	}
-
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	/* Test the function with different characters */
-	char test_chars[] = {'A', 'a', 'Z', '1', '!', 'M'};
-	size_t i;
-
-	for (i = 0; i < sizeof(test_chars) / sizeof(test_chars[0]); i++)
-	{
-	putchar(test_chars[i]); /* Print the character being tested */
-	putchar(':');           /* Print a separator */
-	_isupper(test_chars[i]); /* Call the _isupper function */
-	putchar('\n');         /* New line for better output readability */
-	}
-
-	return (0);
+		return (0); /* c is not lowercase */
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,5 +5,5 @@
  * Auth: KANGA KOUAKOU BRICE
  * Desc: Header file containing declarations for all functions
  */
-
+int _islower(int c);
 #endif/* main_h */
