0afddff8b0fbbc4e54ee4984045c660918388ad3
kanga-prog <9952@holbertonstudents.com>
2024-10-29 09:07:50 +0100

    EXO 0

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,9 +1,8 @@
 #ifndef main_h
 #define main_h
-/*
- * File: main.h
- * Auth: KANGA KOUAKOU BRICE
- * Desc: Header file containing declarations for all functions
- */
+
+
 int _isupper(int c);
-#endif/* main_h */
+
+
+#endif
