79ecb6be8e7b6b9c975657a69a3a9f330c11044f
kanga-prog <9952@holbertonstudents.com>
2024-10-30 09:23:21 +0100

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of size `size` using the '#' character.
+ * @size: The size of the triangle.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
