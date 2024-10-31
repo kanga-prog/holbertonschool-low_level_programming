18f81f0cc63759e4d6c23f7172c70e8d371dd796
kanga-prog <9952@holbertonstudents.com>
2024-10-31 13:28:13 +0100

    3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,18 +1,15 @@
-#include <unistd.h>  // For the write function
 #include <stdio.h>
 
 /**
  * _puts - Prints a string followed by a new line.
  * @str: Pointer to the string to be printed.
- *
- * Return: Nothing.
  */
 void _puts(char *str)
 {
-	while (*str != '\0')  /* Iterate through each character of the string */
+	while (*str != '\0')
 	{
-		write(1, str, 1);  /* Write the current character to stdout */
-		str++;              /* Move to the next character */
+		putchar(*str);
+		str++;
 	}
-	write(1, "\n", 1);      /* Write a new line at the end */
+	putchar('\n');
 }
