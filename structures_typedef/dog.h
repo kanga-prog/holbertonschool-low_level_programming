6d25025a02a09ffa1aa41db69e9c23663805ab53
kanga-prog <9952@holbertonstudents.com>
2024-11-17 15:53:44 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - A structure to represent a dog.
- * @name: Pointer to a string representing the dog's name.
- * @age: The dog's age (in years).
- * @owner: Pointer to a string representing the dog's owner.
- *
- * Description: This structure is used to store information about a dog,
- * including its name, age, and owner's name.
- */
+/* Définition de la structure dog */
 typedef struct dog
 {
-	char *name;   /* Pointer to the dog's name */
-	float age;    /* The dog's age */
-	char *owner;  /* Pointer to the dog's owner */
+	char *name;   /* Nom du chien */
+	float age;    /* Âge du chien */
+	char *owner;  /* Propriétaire du chien */
 } dog_t;
 
 #endif /* DOG_H */
