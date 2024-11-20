b60ca138700750cc0b62e193a2ff725ec249f738
kanga-prog <9952@holbertonstudents.com>
2024-11-20 07:00:29 +0100

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,44 +1,45 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _strdup - Retourne un pointeur vers une nouvelle chaîne,copie de chaîne.
+ * _strdup - Returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter.
+ * @str: The string to be duplicated.
  *
- * @str: La chaîne à dupliquer.
- *
- * Return: Un pointeur vers la nouvelle chaîne, ou NULL en cas d'erreur.
+ * Return: A pointer to the duplicated string, or NULL if memory
+ *         allocation fails or str is NULL.
  */
 char *_strdup(char *str)
 {
-	unsigned int len;
-	char *new_str;
-	unsigned int i;
+	char *dup_str;
+	int i, len = 0;
 
-	/* Vérifier si la chaîne d'entrée est NULL*/
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	/* Trouver la longueur de la chaîne*/
-	len = strlen(str);  /*Utilisation de strlen pour simplifier*/
+	/* Calculate the length of the string*/
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-	new_str = (char *)malloc((len + 1) * sizeof(char));
-
-	/* Vérifier si l'allocation de mémoire a échoué*/
-	if (new_str == NULL)
+	/*Allocate memory for the new string (+1 for the null terminator)*/
+	dup_str = malloc((len + 1) * sizeof(char));
+	if (dup_str == NULL)
 	{
 		return (NULL);
 	}
 
+	/* Copy the string into the newly allocated memory*/
 	for (i = 0; i < len; i++)
 	{
-		new_str[i] = str[i];
+		dup_str[i] = str[i];
 	}
 
-	/*Ajouter le caractère NULL à la fin de la nouvelle chaîne*/
-	new_str[len] = '\0';
+	/* Add the null terminator*/
+	dup_str[len] = '\0';
 
-	return (new_str);
+	return (dup_str);
 }
+
