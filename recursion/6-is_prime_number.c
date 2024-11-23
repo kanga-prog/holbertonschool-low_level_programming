ac36acd8523e4d04874ead283f9a637063e23053
kanga-prog <9952@holbertonstudents.com>
2024-11-23 16:36:06 +0100

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,39 +1,40 @@
-#include <stdio.h>
-#include <math.h>
+#include "main.h"
 
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to check.
+ * check_prime - helper function to check if a number is prime recursively.
+ * @n: number to check.
+ * @i: current divisor to check against.
  *
- * Return: 1 if n is a prime number, otherwise 0.
+ * Return: 1 if prime, 0 otherwise.
+ */
+int check_prime(int n, int i)
+{
+	if (i * i > n)
+	return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (check_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the number is prime, otherwise returns 0.
+ * @n: number to check.
+ *
+ * Return: 1 if prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-	int i;  /* Déclaration de la variable 'i' au début de la fonction*/
-
 	if (n <= 1)
-	{
 		return (0);
-	}
 
-	if (n <= 3)
-	{
+	if (n == 2)
 		return (1);
-	}
 
-	if (n % 2 == 0 || n % 3 == 0)
-	{
+	if (n % 2 == 0)
 		return (0);
-	}
 
-	/* Check divisibility from 5 to sqrt(n) */
-	for (i = 5; i * i <= n; i += 6)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-		{
-			return (0);
-		}
-	}
-
-	return (1);
+	return (check_prime(n, 3));
 }
+
