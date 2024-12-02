4915b104e9b17d709780fd24a5bc5ec9b238e9d0
kanga-prog <9952@holbertonstudents.com>
2024-12-02 20:18:20 +0100

    0-print_list.c 2

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,32 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/* Définition de la structure d'un nœud de liste chaînée*/
-struct Node {
-    int data;           /* Donnée stockée dans le nœud*/
-    struct Node* next;  /* Pointeur vers le nœud suivant*/
-};
+#include <stdlib.h>
 
-/* Fonction pour afficher tous les éléments de la liste*/
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* Function prototype */
 size_t print_list(const list_t *h);
 
-
-
-/* Fonction pour ajouter un élément au début de la liste*/
-void push(struct Node** head_ref, int new_data);
-
-/* Fonction pour supprimer le premier élément de la liste*/
-void pop(struct Node** head_ref);
-
-
-/* Fonction pour rechercher un élément dans la liste*/
-int search(struct Node* head, int key);
-
-/* Fonction pour supprimer un élément spécifique de la liste*/
-void deleteNode(struct Node** head_ref, int key);
-
-/* Fonction pour libérer toute la mémoire allouée pour la liste*/
-void freeList(struct Node* head);
-
-#endif
+#endif /* LISTS_H */
 
