e29db05fc06140128218d5eb15d832a98d6c19ba
kanga-prog <9952@holbertonstudents.com>
2024-12-02 22:52:25 +0100

    2-add_node.c1

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,8 +5,8 @@
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Pointer to the head of the list.
- * @str: The string to be added to the new node.
+ * @head: Double pointer to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
  *
  * Return: The address of the new element, or NULL if it failed.
  */
@@ -18,27 +18,24 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-		return (NULL);  /* If memory allocation fails, return NULL */
+		return (NULL);  /* Return NULL if memory allocation fails */
 	}
 
 	/* Duplicate the string and assign it to the new node */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free(new_node);  /* If strdup fails, free the node */
-		return (NULL);
+		free(new_node);  /* Free the node if strdup fails */
+		return (NULL);    /* Return NULL if strdup fails */
 	}
 
 	/* Set the length of the string */
 	new_node->len = strlen(str);
 
-	/* Set the next pointer to the current head of the list */
-	new_node->next = *head;
+	/* Insert the new node at the beginning of the list */
+	new_node->next = *head;  /* Point the new node to the current head */
+	*head = new_node;        /* Update the head to point to the new node */
 
-	/* Update the head pointer to point to the new node */
-	*head = new_node;
-
-	/* Return the address of the new node */
-	return (new_node);
+	return (new_node);        /* Return the new node */
 }
 
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,6 +19,8 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+
+void _putchar(char c);
 /* Function prototype */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
