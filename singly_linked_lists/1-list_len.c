fcf3ec22327621fa7ab4400e823330a9a39e1831
kanga-prog <9952@holbertonstudents.com>
2024-12-02 21:47:19 +0100

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a list_t list.
+ * @h: Pointer to the first node of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	/* Traverse through the linked list */
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return count;
+}
+
