e3739c06dce8775e170339425d80d4b6cf5c6e97
kanga-prog <9952@holbertonstudents.com>
2024-12-04 13:00:07 +0100

    7-insert_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,34 +1,33 @@
 #include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
- * @head: A pointer to the pointer to the head of the list.
- * @n: The integer to store in the new node.
- *
- * Return: The address of the new node, or NULL if it fails.
+ * add_dnodeint - Adds a new node at the beginning of a doubly linked list.
+ * @head: A pointer to the head of the list.
+ * @n: The integer value to insert into the new node.
+ * 
+ * Return: The address of the new node, or NULL if it failed.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	/* Allocate memory for the new node */
+	/* Allocate memory for the new node*/
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node) /* Memory allocation failed */
-		return (NULL);
+	if (new_node == NULL)
+		return NULL;
 
-	/* Initialize the new node */
+	/* Initialize the new node*/
 	new_node->n = n;
-	new_node->prev = NULL; /* New node will be the first, so prev is NULL */
-	new_node->next = *head; /* Next points to the current head */
+	new_node->prev = NULL;  /* Since it's the new head, no previous node*/
+	new_node->next = *head; /* Point to the current head (could be NULL)*/
 
-	/* If the list is not empty, update the previous head's prev pointer */
-	if (*head)
+	if (*head != NULL)
 		(*head)->prev = new_node;
 
-	/* Update the head to the new node */
+	/* Update the head pointer to point to the new node*/
 	*head = new_node;
 
-	return (new_node); /* Return the address of the new node */
+	return new_node;  /* Return the address of the new node*/
 }
-
