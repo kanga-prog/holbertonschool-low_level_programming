99c53f2f8a613fb86d5a708de7ad2cba5c76b705
kanga-prog <9952@holbertonstudents.com>
2024-12-06 23:18:11 +0100

    0-binary_to_uint.c correction

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,40 @@
-#include <stddef.h>
+#include "main.h"
 
 /**
- * binary_to_uint - Convertit une chaîne binaire en un entier non signé.
- * @b: La chaîne binaire à convertir.
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: b is pointing to a string of 0 and 1 chars
  *
- * Return: Le nombre entier correspondant en base 10, ou 0 si
- *         la chaîne est invalide ou NULL.
+ * Return: Converted number or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * or if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int i;
+	unsigned int res = 0, one, i;
+	int l = 0;
 
-	/* If the string is NULL, return 0*/
-	if (b == NULL)
+	if (b == 0)
 		return (0);
 
-	/* Loop through the string*/
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		/* If the character is not '0' or '1', return 0*/
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
 
-		result = result * 2 + (b[i] - '0');
+	while (b[l] != 0)
+	{
+		if (b[l] != '0' && b[l] != '1')
+			return (0);
+		l++;
 	}
 
-	return (result);
+	l = l - 1;
+
+	for (i = 0; l >= 0; i++, l--)
+	{
+		if (b[l] == '0')
+			one = 0;
+		else if (b[l] == '1')
+			one = 1;
+		one = one << i;
+		res = res | one;
+	}
+	return (res);
 }
 
