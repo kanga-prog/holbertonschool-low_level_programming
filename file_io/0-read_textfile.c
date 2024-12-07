772e93f1f4e6fa89578d7ffa474fff4cb22ea4fe
kanga-prog <9952@holbertonstudents.com>
2024-12-07 23:34:17 +0100

    add 0-read_textfile.c revu

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,63 +1,51 @@
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * read_textfile - Lit un fichier texte et l'affiche à la sortie standard
- * @filename: Le nom du fichier à lire.
- * @letters: Le nombre de lettres à lire et à afficher depuis le fichier.
+ * read_textfile -  reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
  *
- * Description: Cette fonction ouvre un fichier en mode lecture seule, lit
- * un nombre spécifié de caractères du fichier et les affiche sur la sortie
- * standard. Si une erreur se produit (par exemple, si le fichier ne peut pas
- * être ouvert, si la lecture ou l'écriture échoue), elle retourne 0.
- * Elle retourne le nombre réel de caractères lus et affichés cas d'succès.
- *
- * Return: Le nombre réel de caractères lus et affichés, ou 0,cas d'erreur.
+ * Return: returns the actual number of letters it could read and print
+ * 0 if file cannot be opened or read
+ * 0 if filename is NULL
+ * 0 if write fails or does not write expected amount of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	int fd, checkr, checkw;
+	char *c;
 
-
-	if (filename == NULL)
+	if (filename == 0)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	c = malloc(letters + 1);
+
+	if (c == 0)
+		return (0);
+
+	fd  = open(filename, O_RDONLY);
+
 	if (fd == -1)
-		return (0);
+		return (free(c), 0);
 
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+	checkr = read(fd, c, letters);
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+	if (checkr == -1)
+		return (free(c), 0);
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+	c[letters] = '\0';
 
-	free(buffer);
+	checkw = write(STDOUT_FILENO, c, checkr);
+	if (checkw == -1)
+		return (free(c), 0);
+
+	free(c);
 	close(fd);
-
-	return (bytes_written);
+	return (checkw);
 }
-
