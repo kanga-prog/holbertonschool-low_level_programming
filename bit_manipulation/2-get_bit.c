43acb1156e5e07b433b1e043dc33fa8493399a17
kanga-prog <9952@holbertonstudents.com>
2024-12-07 12:58:28 +0100

    2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: the number to check
+ * @index: the index of the bit to retrieve, starting from 0
+ *
+ * Return: the value of the bit at index or -1 if an error occurred
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	/* Check if the index is out range for an unsigned long int (norm 64 b)*/
-	if (index >= sizeof(n) * 8) {
+	if (index >= sizeof(n) * 8)
+	{
 		return (-1);  /* Error: index is out of range*/
 	}
 
